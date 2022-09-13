10fbd966c94cedd73bdd065e0a94b8fba08eb730
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-09-13 13:10:21 +0700

    Update ContestRow.swift

--- CP/ContestRow.swift
+++ CP/ContestRow.swift
@@ -3,12 +3,25 @@ import SwiftUI
 struct ContestRow : View {
     var contest: Contest
     
+    //    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    func getDate(cur_date: String)->Date {
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatterGet.timeZone = TimeZone(abbreviation: "UTC")
+        
+        guard let date = dateFormatterGet.date(from: cur_date) else { return Date() }
+        return date
+    }
+    
     func convertDatetime(cur_date: String)->String {
         let dateFormatterGet = DateFormatter()
         dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-
+        dateFormatterGet.timeZone = TimeZone(abbreviation: "UTC")
+        
         let dateFormatterPrint = DateFormatter()
         dateFormatterPrint.dateFormat = "MMM dd,yyyy EEE HH:mm"
+        //        dateFormatterPrint.timeZone = TimeZone(abbreviation: "UTC")
         
         var result:String
         
@@ -45,34 +58,77 @@ struct ContestRow : View {
         }
         return result
     }
-
+    
     var body: some View {
         VStack {
             HStack {
-                Text("event:")
+                Text("Event:")
                 Spacer()
-                Text(contest.event)
+                Text(verbatim: contest.event)
+                    .font(.headline)
             }
             HStack {
-                Text("duration:")
+                Text("Duration:")
                 Spacer()
                 Text(verbatim: convertDuration(seconds: contest.duration))
             }
             HStack {
-                Text("start:")
+                Text("Start:")
                 Spacer()
                 Text(verbatim: convertDatetime(cur_date: contest.start))
             }
             HStack {
-                Text("end:")
+                Text("End:")
                 Spacer()
                 Text(verbatim: convertDatetime(cur_date: contest.end))
             }
             HStack {
-                Text("host:")
+                Text("Host:")
                 Spacer()
                 Text(verbatim: contest.host)
             }
+            HStack {
+                Text("Time left:")
+                Spacer()
+                TimerView(setDate: getDate(cur_date: contest.start))
+            }
         }
+    }
+}
+
+struct TimerView: View {
+    @State var nowDate:Date = Date()
+    let setDate: Date
+    
+    var timer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+            self.nowDate = Date()
+        }
+    }
+    
+    var body: some View {
+        Text(TimerFunction(from: setDate))
+            .onAppear(perform: {self.timer
+            })
+    }
+    
+    func TimerFunction(from date: Date) -> String {
+        let calendar = Calendar(identifier: .gregorian)
+        let timeValue = calendar
+            .dateComponents([.day, .hour, .minute, .second], from: nowDate, to: setDate)
+        
+        if timeValue.day ?? 0 > 0 {
+            return String(format: "%d days %02d:%02d:%02d",
+                          timeValue.day!,
+                          timeValue.hour!,
+                          timeValue.minute!,
+                          timeValue.second!)
+        } else {
+            return String(format: "%02d:%02d:%02d",
+                          timeValue.hour!,
+                          timeValue.minute!,
+                          timeValue.second!)
+        }
+        
     }
 }
